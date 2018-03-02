488e4e62baa6858da0f235c29490c769c70460a3
kaleidostoni <kaleidostoni@gmail.com>
2018-03-02 09:32:41 -0600

    subiendo funcion para pintar los resultados

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
+const container = $('#container');
+
 function showPokemons(pokemon){
-    console.log(pokemon);
+//console.log(pokemon);
+const allPokemons = pokemon.pokemon_entries;
+//console.log(allPokemons);
+let template = " "
+allPokemons.forEach(item =>{
+    const pokemonName = item.pokemon_species.name;
+     console.log(pokemonName);
+    const pokemonURL = item.pokemon_species.url;
+    console.log(pokemonURL);
+ template += ``
+
+});
 }
 
-$('#search-form').submit(function (e) {
-    e.preventDefault;
 
-    const input = $('#search-input');
-    const inputValue = input.val();
-    console.log($inputValue);
 
-    $ajax({
+    $.ajax({
         url:
-            `https://pokeapi.co/docsv2/${inputValue}`
+            `https://pokeapi.co/api/v2/pokedex/1`
     }).done(showPokemons);
-});
 
